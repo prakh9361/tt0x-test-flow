1df5d26578e11572dab0eddbc83e7d0b34bdc70a
prakh9361 <prakh9361@gmail.com>
2026-01-21 11:19:34 +0530

    reg implementation

--- src/project.v
+++ src/project.v
@@ -17,11 +17,16 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ~ui_in;   // Example: ou_out is the inverse of ui_in
+  //assign uo_out  = ~ui_in;   // Example: ou_out is the inverse of ui_in
   assign uio_out = 0;
   assign uio_oe  = 0;
-
+  reg [7:0] d;
+	
+  always(@ posedge clk) begin
+  	d <= ui_in;
+	uo_out <= d;
+  end
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, rst_n, 1'b0};
 
 endmodule
