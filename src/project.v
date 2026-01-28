7304e1925e0f54a16287f6e1817695551fd4383d
prakh9361 <prakh9361@gmail.com>
2026-01-28 16:51:06 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_jnav_generator (
+module tt_um_example (
     input  wire [7:0] ui_in,    // Dedicated inputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uo_out,   // Dedicated outputs
